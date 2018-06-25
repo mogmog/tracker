c7d001ec811736f38a3d257d809fa85ffaeedd3f
Graham <mogmog@gmail.com>
2018-06-25 15:24:20 +0100

    sql

---
+++ api/tracker_public_cardposition.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.cardposition (id, pageid, userid, position, layoutid, cardid, key) VALUES (11, 4, 1, 2, 1, 1160938, '{"id": 1, "type": "question"}');
+INSERT INTO public.cardposition (id, pageid, userid, position, layoutid, cardid, key) VALUES (14, 4, 1, 5, 1, 1160941, '{"id": 1, "type": "question"}');
+INSERT INTO public.cardposition (id, pageid, userid, position, layoutid, cardid, key) VALUES (13, 4, 1, 4, 1, 1160940, '{"id": 1, "type": "question"}');
+INSERT INTO public.cardposition (id, pageid, userid, position, layoutid, cardid, key) VALUES (12, 4, 1, 3, 1, 1160939, '{"id": 1, "type": "question"}');
+INSERT INTO public.cardposition (id, pageid, userid, position, layoutid, cardid, key) VALUES (15, 4, 1, 12, 1, 87, '{"id": 1, "type": "question"}');
+INSERT INTO public.cardposition (id, pageid, userid, position, layoutid, cardid, key) VALUES (16, 4, 1, 13, 1, 111, '{"id": 1, "type": "question"}');
+INSERT INTO public.cardposition (id, pageid, userid, position, layoutid, cardid, key) VALUES (17, 4, 1, 0, 1, 97, '{"id": 1, "type": "question"}');
---
+++ api/tracker_public_cards.sql
@@ -0,0 +1,54 @@
+INSERT INTO public.cards (id, component, key, data) VALUES (1160939, 'NarrativeCard', '{"id": 1, "type": "question"}', '{"color": "5px solid rgba(79, 120, 34, .4)", "trend": [{"x": "2018-06-21", "y": 1}, {"x": "2018-06-22", "y": 6}, {"x": "2018-06-23", "y": 4}, {"x": "2018-06-24", "y": 8}, {"x": "2018-06-25", "y": 3}, {"x": "2018-06-26", "y": 7}, {"x": "2018-06-27", "y": 2}], "posts": [{"url": "https://www.facebook.com/20531316728/posts/10154009990506729/"}, {"url": "https://www.facebook.com/20531316728/posts/10154009990506729/"}, {"url": "https://www.facebook.com/20531316728/posts/10154009990506729/"}], "direction": "up", "influencers": [{"name": "Mr fgfgfgfg", "twitter": true, "facebook": true, "instagram": true}, {"name": "Mr person 2", "twitter": false, "facebook": true, "instagram": false}, {"name": "Mr person 3", "twitter": true, "facebook": false, "instagram": true}], "socialmedia": {"reach": {"type": "Reach", "delta": "+12 above baseline", "absolute": 12, "percent": 12, "direction": "down"}, "posts": {"type": "Posts", "delta": "+12 above baseline", "absolute": 12, "percent": 12, "direction": "up"}, "engagement": {"type": "Engagement", "delta": "+12 above baseline", "absolute": 56, "percent": 12, "direction": "up"}}, "subtitle": "lorum dipsum", "title": "Soviet Nostalgia"}');
+INSERT INTO public.cards (id, component, key, data) VALUES (1160940, 'NarrativeCard', '{"id": 1, "type": "question"}', '{"color": "5px solid rgba(30, 30, 90, .4)", "trend": [{"x": "2018-06-21", "y": 1}, {"x": "2018-06-22", "y": 6}, {"x": "2018-06-23", "y": 4}, {"x": "2018-06-24", "y": 8}, {"x": "2018-06-25", "y": 3}, {"x": "2018-06-26", "y": 7}, {"x": "2018-06-27", "y": 2}], "posts": [{"url": "https://www.facebook.com/llbean/posts/10155950642862415"}, {"url": "https://www.facebook.com/20531316728/posts/10154009990506729/"}, {"url": "https://www.facebook.com/20531316728/posts/10154009990506729/"}], "direction": "up", "influencers": [{"name": "Mr person", "twitter": true, "facebook": true, "instagram": true}, {"name": "Mr person 2", "twitter": false, "facebook": true, "instagram": false}, {"name": "Mr person 3", "twitter": true, "facebook": false, "instagram": true}], "socialmedia": {"reach": {"type": "Reach", "direction": "down", "absolute": 12, "percent": 12, "delta": "+12 above baseline"}, "posts": {"type": "Posts", "direction": "down", "absolute": 12, "percent": 12, "delta": "+12 above baseline"}, "engagement": {"type": "Engagement", "direction": "down", "absolute": 12, "percent": 12, "delta": "+12 above baseline"}}, "subtitle": "blah!", "title": "World Cup propoganda"}');
+INSERT INTO public.cards (id, component, key, data) VALUES (1160941, 'NarrativeCard', '{"id": 1, "type": "question"}', '{"color": "5px solid rgba(90, 90, 90, .4)", "trend": [{"x": "2018-06-21", "y": 1}, {"x": "2018-06-22", "y": 6}, {"x": "2018-06-23", "y": 4}, {"x": "2018-06-24", "y": 8}, {"x": "2018-06-25", "y": 3}, {"x": "2018-06-26", "y": 7}, {"x": "2018-06-27", "y": 2}], "posts": [{"url": "https://www.facebook.com/20531316728/posts/10154009990506729/"}, {"url": "https://www.facebook.com/20531316728/posts/10154009990506729/"}, {"url": "https://www.facebook.com/20531316728/posts/10154009990506729/"}], "direction": "up", "influencers": [{"name": "Mr person", "twitter": true, "facebook": true, "instagram": true}, {"name": "Mr person 2", "twitter": false, "facebook": true, "instagram": false}, {"name": "Mr person 3", "twitter": true, "facebook": false, "instagram": true}], "socialmedia": {"reach": {"type": "Reach", "delta": "+12 above baseline", "absolute": 12, "percent": 12, "direction": "up"}, "posts": {"type": "Posts", "delta": "+12 above baseline", "absolute": 12, "percent": 12, "direction": "up"}, "engagement": {"type": "Engagement", "delta": "+12 above baseline", "absolute": 12, "percent": 12, "direction": "up"}}, "subtitle": "ipso dipsum", "title": "Putin riding animals"}');
+INSERT INTO public.cards (id, component, key, data) VALUES (1160937, 'NarrativeSummaryCard', '{"id": 1, "type": "question"}', '{"title": "New Narrative Summary", "values": [{"value": "124", "title": "thing"}, {"value": "93%", "title": "third"}, {"value": "94%", "title": "third"}]}');
+INSERT INTO public.cards (id, component, key, data) VALUES (87, 'InfluencerListCard', '{"id": 1, "type": "question"}', '{
+  "title" : "Influencer list",
+  "influencers" : [
+  {
+    "name": "Mr person",
+    "facebook" : true,
+    "twitter" : true,
+    "instagram" : true
+  },
+  {
+    "name": "Mr person 2",
+    "facebook" : true,
+    "twitter" : false,
+    "instagram" : false
+  },
+  {
+    "name": "Mr person 3",
+    "facebook" : false,
+    "twitter" : true,
+    "instagram" : true
+  }
+]
+}
+
+');
+INSERT INTO public.cards (id, component, key, data) VALUES (111, 'VideoChartCard', '{"id": 1, "type": "question"}', '{"socialmedia":[
+      {"country": "Instagram", "year": "1750", "value": 300},
+      {"country": "Instagram", "year": "1800", "value": 350},
+      {"country": "Instagram", "year": "1850", "value": 450},
+      {"country": "Instagram", "year": "1900", "value": 550},
+      {"country": "Instagram", "year": "1950", "value": 660},
+      {"country": "Instagram", "year": "1999", "value": 1000},
+      {"country": "Instagram", "year": "2050", "value": 1200},
+      {"country": "Facebook", "year": "1750", "value": 106},
+      {"country": "Facebook", "year": "1800", "value": 107},
+      {"country": "Facebook", "year": "1850", "value": 111},
+      {"country": "Facebook", "year": "1900", "value": 133},
+      {"country": "Facebook", "year": "1950", "value": 221},
+      {"country": "Facebook", "year": "1999", "value": 767},
+      {"country": "Facebook", "year": "2050", "value": 1766},
+      {"country": "Twitter", "year": "1750", "value": 500},
+      {"country": "Twitter", "year": "1800", "value": 400},
+      {"country": "Twitter", "year": "1850", "value": 300},
+      {"country": "Twitter", "year": "1900", "value": 200},
+      {"country": "Twitter", "year": "1950", "value": 100},
+      {"country": "Twitter", "year": "1999", "value": 50},
+      {"country": "Twitter", "year": "2050", "value": 25}
+    ]}');
+INSERT INTO public.cards (id, component, key, data) VALUES (97, 'CountryInfoCard', '{"id": 1, "type": "question"}', '{"title": "CountryLand", "subtitle": "I am subtitle", "map": {"features": [{"properties": {"area": 1150180, "name": "Bermuda Triangle"}, "geometry": {"coordinates": [[[-64.73, 32.31], [-80.19, 25.76], [-66.09, 18.43], [-64.73, 32.31]]], "type": "Polygon"}, "type": "Feature"}], "type": "FeatureCollection"}}');
+INSERT INTO public.cards (id, component, key, data) VALUES (1160938, 'NarrativeCard', '{"id": 1, "type": "question"}', '{"color": "5px solid rgba(255, 00, 0, .4)", "trend": [{"x": "2018-06-21", "y": 1}, {"x": "2018-06-22", "y": 6}, {"x": "2018-06-23", "y": 4}, {"x": "2018-06-24", "y": 8}, {"x": "2018-06-25", "y": 3}, {"x": "2018-06-26", "y": 7}, {"x": "2018-06-27", "y": 2}], "posts": [{"url": "https://www.facebook.com/20531316728/posts/10154009990506721/"}, {"url": "https://www.facebook.com/20531316728/posts/10154009990506729/"}, {"url": "https://www.facebook.com/20531316728/posts/10154009990506729/"}], "direction": "up", "influencers": [{"name": "Mr person", "twitter": true, "facebook": true, "instagram": true}, {"name": "Mr person 2", "twitter": false, "facebook": true, "instagram": false}, {"name": "Mr person 3", "twitter": true, "facebook": false, "instagram": true}], "socialmedia": {"reach": {"type": "Reach", "direction": "up", "absolute": 12, "percent": 12, "delta": "+12 above baseline"}, "posts": {"type": "Posts", "direction": "down", "absolute": 12, "percent": 12, "delta": "+12 above baseline"}, "engagement": {"type": "Engagement", "direction": "up", "absolute": 12, "percent": 12, "delta": "+12 above baseline"}}, "subtitle": "ipso factum", "title": "Anti West Sentiment"}');
