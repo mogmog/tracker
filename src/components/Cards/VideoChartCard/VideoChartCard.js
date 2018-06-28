25c6602c13c75098ed4e63bffe9398edff2f4364
Graham <mogmog@gmail.com>
2018-06-28 15:24:52 +0100

    graph tweaks

--- src/components/Cards/VideoChartCard/VideoChartCard.js
+++ src/components/Cards/VideoChartCard/VideoChartCard.js
@@ -21,52 +21,67 @@ class VideoChartCard extends Component {
   }
 
   render() {
-    const { data } = this.props;
+    const { data2 } = this.props;
 
-    const dv = new Something().source(data.socialmedia);
-
-    dv.transform({
-      type: 'percent',
-      field: 'value',
-      dimension: 'country',
-      groupBy: [ 'year' ],
-      as: 'percent'
-    });
+    const data = [
+      {country: 'Anti-West Sentiment', year: '2011', value: 502},
+      {country: 'Anti-West Sentiment', year: '2012', value: 635},
+      {country: 'Anti-West Sentiment', year: '2013', value: 809},
+      {country: 'Anti-West Sentiment', year: '2014', value: 5268},
+      {country: 'Anti-West Sentiment', year: '2015', value: 4400},
+      {country: 'Anti-West Sentiment', year: '2016', value: 3634},
+      {country: 'Anti-West Sentiment', year: '2017', value: 947},
+      {country: 'Soviet Nostalgia', year: '2011', value: 106},
+      {country: 'Soviet Nostalgia', year: '2012', value: 107},
+      {country: 'Soviet Nostalgia', year: '2013', value: 111},
+      {country: 'Soviet Nostalgia', year: '2014', value: 1766},
+      {country: 'Soviet Nostalgia', year: '2015', value: 221},
+      {country: 'Soviet Nostalgia', year: '2016', value: 767},
+      {country: 'Soviet Nostalgia', year: '2017', value: 133},
+      {country: 'Pro-Russia Sentiment', year: '2011', value: 163},
+      {country: 'Pro-Russia Sentiment', year: '2012', value: 203},
+      {country: 'Pro-Russia Sentiment', year: '2013', value: 276},
+      {country: 'Pro-Russia Sentiment', year: '2014', value: 628},
+      {country: 'Pro-Russia Sentiment', year: '2015', value: 547},
+      {country: 'Pro-Russia Sentiment', year: '2016', value: 729},
+      {country: 'Pro-Russia Sentiment', year: '2017', value: 408},
+      {country: 'Undermining Western Cohesion', year: '2011', value: 200},
+      {country: 'Undermining Western Cohesion', year: '2012', value: 200},
+      {country: 'Undermining Western Cohesion', year: '2013', value: 200},
+      {country: 'Undermining Western Cohesion', year: '2014', value: 460},
+      {country: 'Undermining Western Cohesion', year: '2015', value: 230},
+      {country: 'Undermining Western Cohesion', year: '2016', value: 300},
+      {country: 'Undermining Western Cohesion', year: '2017', value: 300},
+    ];
     const cols = {
       year: {
         type: 'linear',
-        tickInterval: 2
-      },
-      'percent': {
-        formatter: function(value) {
-          value = value || 0;
-          value = value * 100;
-          return parseInt(value);
-        },
-        alias: 'percent(%)'
+        tickInterval: 1
       }
     }
 
 
-
+    const colsMark = {
+      year: {
+        type: 'linear',
+        tickInterval: 1
+      }
+    }
 
     var markData = [
-      {"date": "2017-08-06", "type": "Client", "version": "2.0", "value": 1111111},
-      {"date": "2018-08-20", "type": "Client", "version": "2.1", "value": 1111111},
-      {"date": "2018-08-27", "type": "Server", "version": "3.5", "value": 1111111},
-      {"date": "2018-09-03", "type": "Client", "version": "2.2", "value": 1111111}
+      {"year": "2013", "type": "Protest", "value": 5},
+      {"year": "2017", "type": "Protest", "value": 10},
     ];
 
     function formatter(text, item) {
       var point = item.point;
       var type = point['type'];
-      return '<div style="width: 60px;text-align: center;font-size: 8px;line-height: 1.2;color: #fff;margin-left: -8px;"><span>' + type + '</span><br><span>' + text + '</span></div>';
+      return '<div style="width: 60px;text-align: center;font-size: 8px;line-height: 1.2;color: #fff;margin-left: -8px;"><span>' + type + '</span><br><span>' + '' + '</span></div>';
     }
 
     function test() {
-      alert(1);
+      alert(11);
     }
-
 
     return (<ChartCard
       bordered={false}
@@ -75,16 +90,30 @@ class VideoChartCard extends Component {
     >
       <Chart height={400}  scale={cols} forceFit >
 
-        <View data={dv} scale={cols} >
+        {/*<View data={markData} scale={colsMark}>
 
-            <Axis name="year" />
-            <Axis name="percent" />
+          <Geom type="interval" position="year*value" color={['type', ['#ff1f00', '#093']]} size={3} >
+            <Label
+              content="Protest"
+              custom={true}
+              renderer={formatter}
+              offset={0}
+            />
+          </Geom>
 
-            <Tooltip crosshairs={{type:'line'}}/>
-            <Geom type="areaStack" position="year*percent" color='country' />
-            <Geom type="lineStack" position="year*percent" size={2} color='country' />
+        </View>*/}
+
+        <View data={data} scale={cols} >
+
+          <Axis name="year" />
+          <Axis name="value" />
+          <Legend />
+
+          <Geom type="line" position="year*value" size={5} color={['country', ['#1796D2', '#F07534', '#633085', '#17918B']]} />
 
         </View>
+
+        <Tooltip crosshairs={{type:'line'}}/>
 
         <Legend />
 
